6392913b88efdb921c06490fa1f87151392354ca
Omkar <omkarbhusnale23@gmail.com>
2025-04-15 03:43:50 +0530

    Revert "Configuration Update"
    This reverts commit 90bdc302bca6068fd7e085dd234f98b0e79e409a.

--- client-side/vite.config.ts
+++ client-side/vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
